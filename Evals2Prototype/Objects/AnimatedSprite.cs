513b1ce0a0832c416e0ba8e7cddd36928da37e6a
S00164384 <s00164384@mail.itsligo.ie>
2018-09-03 21:05:28 +0100

    Add project files.

---
+++ Evals2Prototype/Objects/AnimatedSprite.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Evals2Prototype.Objects
+{
+    public class AnimatedSprite : DrawableGameComponent
+    {
+        public Texture2D Image;
+        Texture2D boundtx;
+        public Rectangle BoundingBox;
+        public Vector2 oldPosition;
+        public Vector2 Position;
+        Game game;
+        public SpriteBatch Sb;
+        float speed;
+        public string tag;
+        public bool InCollision = false;
+        public AnimatedSprite collidingWith;
+        Vector2 movement = new Vector2(0, 0);
+
+        //For animating
+        int Frames = 0;
+        int currentFrame = 0;
+        int timeBetweenFrames = 100;
+        float timer = 0f;
+        Rectangle sourceRectangle;
+
+        public AnimatedSprite(Game g,Texture2D tx,Vector2 pos,string t,Texture2D bounds):base(g)
+        {
+            game = g;
+            Visible = true;
+            tag = t;
+            boundtx = bounds;
+            Image = tx;
+            Position = new Vector2(pos.X,pos.Y);
+            BoundingBox = new Rectangle((int)this.Position.X,(int)this.Position.Y,Image.Width,Image.Height);
+            speed = 5;
+            g.Components.Add(this);
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            oldPosition = Position;
+            BoundingBox = new Rectangle((int)this.Position.X, (int)this.Position.Y, Image.Width, Image.Height);
+
+
+            base.Update(gameTime);
+        }
+
+        public bool Collision(AnimatedSprite other)
+        {
+            BoundingBox = new Rectangle((int)this.Position.X, (int)this.Position.Y, Image.Width, Image.Height);
+            Rectangle OtherBox = new Rectangle((int)other.Position.X, (int)other.Position.Y, other.Image.Width, other.Image.Height);
+            if (BoundingBox.Intersects(OtherBox))
+            {
+                InCollision = true;
+                collidingWith = other;
+                return true;
+            }
+            else
+            {
+                InCollision = false;
+                collidingWith = null;
+                return false;
+            }
+        }
+
+        public void Move(Vector2 movement)
+        {
+            Position += movement;
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            SpriteBatch Sb = game.Services.GetService(typeof(SpriteBatch)) as SpriteBatch;
+            if (Sb == null) return;
+            Sb.Begin(SpriteSortMode.Immediate);
+            Sb.Draw(Image, Position, Color.White);
+            Sb.Draw(boundtx, BoundingBox, Color.White);
+            Sb.End();
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Evals2Prototype/Objects/Player.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Evals2Prototype.Objects
+{
+    class Player :AnimatedSprite
+    {
+        Vector2 movement;
+        bool grounded;
+        bool onSide;
+        Vector2 oldPosition;
+        List<AnimatedSprite> floors;
+        public Player(Game g, Texture2D tx, Vector2 pos,Texture2D bounds,List<AnimatedSprite> f) : base(g,tx,pos,"player",bounds)
+        {
+            floors = f;
+            grounded = false;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            oldPosition = Position;
+            bool jump = false;
+            if (Keyboard.GetState().IsKeyDown(Keys.Left) && !onSide)
+            {
+                movement.X += -1;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Right) && !onSide)
+            {
+                movement.X += 1;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Up) && grounded)
+            {
+                movement.Y -= 10;
+                grounded = false;
+                jump = true;
+            }
+
+            if (movement.X > 5)
+                movement.X = 5;
+            if (movement.X < -5)
+                movement.X = -5;
+
+            if (movement.Y < -15)
+                movement.Y = -15;
+        
+
+
+
+
+
+            if (movement.X > 0)
+                movement.X -= 0.2f;
+            if (movement.X < 0)
+                movement.X += 0.2f;
+
+            if(!grounded)
+                movement.Y += .5f;
+
+            Rectangle predictiveMovementX = new Rectangle((int)Position.X + (int)movement.X, (int)Position.Y, Image.Width, Image.Height);
+            Rectangle predictiveMovementY = new Rectangle((int)Position.X, (int)Position.Y + (int)movement.Y, Image.Width, Image.Height);
+
+            foreach (AnimatedSprite a in floors)
+            {
+                if (predictiveMovementY.Intersects(a.BoundingBox))
+                {
+                    Position.Y = oldPosition.Y;
+                    movement.Y = 0;
+                    grounded = true;
+                }
+                if(predictiveMovementX.Intersects(a.BoundingBox))
+                {
+                    Position.X = oldPosition.X;
+                    movement.X = 0;
+                }
+                else
+                {
+                    grounded = false;
+                }
+            }
+
+            Move(movement);
+
+
+            //Position.Y = collidingWith.BoundingBox.Top - (Image.Height); 
+            //}
+            //else
+            //{
+            //    onSide = false;
+            //    grounded = false;
+            //}
+
+
+
+
+            base.Update(gameTime);
+        }
+
+
+    }
+}
